10aaa29e630b1a8f2cf3e059cea312572283cbe5
juliomutley <913772+juliomutley@users.noreply.github.com>
2024-07-12 20:28:55 +0000

    fix path refresh

--- Prepare-D365DevelopmentMachine.ps1
+++ Prepare-D365DevelopmentMachine.ps1
@@ -19,7 +19,7 @@
 Get-Process devenv | Stop-Process -ErrorAction Ignore
 dotnet nuget add source "https://api.nuget.org/v3/index.json" --name "nuget.org"
 dotnet tool update -g dotnet-vs
-$env:Path = Expand-EnvironmentVariablesRecursively([System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"))
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 vs update --all
 #endregion
 
@@ -271,7 +271,7 @@ $Module2Service | ForEach-Object {
 
 Install-D365SupportingSoftware -Name "7zip" , "adobereader" , "azure-cli" , "azure-data-studio" , "azurepowershell" , "dotnetcore" , "fiddler" , "git.install", "notepadplusplus.install" , "p4merge" , "postman" , "sysinternals" , "vscode", "winmerge"
 
-$env:Path = Expand-EnvironmentVariablesRecursively([System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"))
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 #region vscode extensions
 $vsCodeExtensions = @(
